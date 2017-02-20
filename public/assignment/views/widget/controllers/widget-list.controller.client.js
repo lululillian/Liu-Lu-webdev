720ccd886255d0a37e96b33da48b2c148bf04323
Lu Liu <lululillian@Lus-MacBook-Air.local>
2017-02-19 19:31:00 -0500

    update9

--- public/assignment/views/widget/controllers/widget-list.controller.client.js
+++ public/assignment/views/widget/controllers/widget-list.controller.client.js
@@ -5,24 +5,22 @@
 
     function WidgetListController($sce, $routeParams, WidgetService) {
         var vm = this;
-        vm.doYouTrustUrl = doYouTrustUrl;
-        vm.getTrustedHtml = getTrustedHtml;
-        vm.getWidgetTemplateUrl = getWidgetTemplateUrl;
+
         vm.userId = $routeParams.uid;
         vm.websiteId = $routeParams.wid;
         vm.pageId = $routeParams.pid;
         vm.widgets = WidgetService.findAllWidgets(vm.pageId);
 
-        function getWidgetTemplateUrl(widgetType) {
+        vm.getWidgetTemplateUrl = function (widgetType) {
             var url = 'views/widget/templates/widget-'+widgetType+'.view.client.html';
             return url;
         }
 
-        function getTrustedHtml(html) {
+        vm.getTrustedHtml  = function (html) {
             return $sce.trustAsHtml(html);
         }
 
-        function doYouTrustUrl(url) {
+        vm.getYouTubeEmbedUrl = function (url) {
             var baseUrl="https://www.youtube.com/embed/";
             var urlParts = url.split('/');
             var id=urlParts[urlParts.length - 1];
